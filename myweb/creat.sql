b3a98dd6952fb7d04d1fca68b44f664463badd60
taogeYT <li_yatao@outlook.com>
2017-05-07 14:16:09 +0800

    整理web

---
+++ myweb/creat.sql
@@ -0,0 +1,55 @@
+drop database if exists awesome;
+
+create database awesome;
+
+use awesome;
+
+create table test(
+id int auto_increment,
+name varchar(100) ,
+password varchar(100),
+created_at real,
+primary key (id));
+
+create table users (
+    `id` varchar(50) not null,
+    `email` varchar(50) not null,
+    `passwd` varchar(50) not null,
+    `admin` bool not null,
+    `name` varchar(50) not null,
+    `image` varchar(500) not null,
+    `created_at` real not null,
+    unique key `idx_email` (`email`),
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+)engine=innodb default charset=utf8;
+
+create table blogs (
+    `id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `user_name` varchar(50) not null,
+    `user_image` varchar(500) not null,
+    `name` varchar(50) not null,
+    `summary` varchar(200) not null,
+    `content` mediumtext not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table comments (
+    `id` varchar(50) not null,
+    `blog_id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `user_name` varchar(50) not null,
+    `user_image` varchar(500) not null,
+    `content` mediumtext not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+insert into test
+(id,name,password)
+values
+(1,'yatao','123123');
